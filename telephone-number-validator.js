044b1eade747f85e03b4195acf01845b7f95d444
Conor <c17730535@mydit.ie>
2021-06-02 21:17:58 +0100

    working solution

--- telephone-number-validator.js
+++ telephone-number-validator.js
@@ -1,5 +1,16 @@
 function telephoneCheck(str) {
-    return true;
-  }
+  let regexPatters = [
+    //555-555-5555 or 555 555 5555
+    /^\d{3}-? ?\d{3}-? ?\d{4}$/,
+    //1 555-555-5555 or 1 555 555 5555
+    /^1 \d{3}-? ?\d{3}-? ?\d{4}$/,
+    //(555)555-5555 or (555) 555-5555
+    /^1? ?\(\d{3}\) ?\d{3}-\d{4}$/,
+    //5555555555
+    /^\d{10}$/,
+  ];
 
-  console.log(telephoneCheck("555-555-5555"));
+  return regexPatters.some((pattern) => pattern.test(str));
+}
+
+telephoneCheck("555-555-5555");
